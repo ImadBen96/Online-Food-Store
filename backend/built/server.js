eb7dc2a2d20c2ac5662ea53cfda6cdbb4b53e7dc
ImadBen96 <ibenomar536@gmail.com>
2024-05-19 00:42:28 +0200

    update sever.ts

--- backend/built/server.js
+++ backend/built/server.js
@@ -11,9 +11,12 @@ var food_router_1 = __importDefault(require("./routers/food.router"));
 var user_router_1 = __importDefault(require("./routers/user.router"));
 var database_config_1 = require("./configs/database.config");
 var order_router_1 = __importDefault(require("./routers/order.router"));
+var path_1 = __importDefault(require("path"));
 (0, database_config_1.dbConnect)();
 var app = (0, express_1.default)();
 app.use(express_1.default.json());
+//app.use('/', express.static('../dist/frontend/browser/index.html'));
+app.use(express_1.default.static(path_1.default.resolve('../../frontend/dist/frontend/browser')));
 app.use((0, cors_1.default)({
     credentials: true,
     origin: ["http://localhost:4200"]
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,17 +5,23 @@ import express, { response } from "express";
 import cors from "cors";
 import foodRouter from "./routers/food.router";
 import userRouter from "./routers/user.router";
+import angularRouter from './routers/angular.router';
 import { dbConnect } from "./configs/database.config";
 import orderRouter from "./routers/order.router";
+import path from "path";
 dbConnect();
 
 const app = express();
 app.use(express.json());
-app.use('/', express.static('dist'))
+//app.use('/', express.static('../dist/frontend/browser/index.html'));
+
+app.use(express.static(path.resolve('../../frontend/dist/frontend/browser')));
+  
 app.use(cors({
     credentials: true,
     origin: ["http://localhost:4200"]
 }));
+
 
 app.use("/api/foods",foodRouter);
 app.use("/api/users",userRouter);
